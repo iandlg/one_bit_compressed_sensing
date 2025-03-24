f8e9f9a35424a00b7ca5333ab7176b448ddc3b41
drairikar <drairikar@tudelft.nl>
2025-03-24 18:35:15 +0100

    Add files via upload
    One-bit LP, BP, and updated main function

--- src/one_bit_lp.m
+++ src/one_bit_lp.m
@@ -1,11 +1,17 @@
-function x_opt = one_bit_lp(y, A)
+function x_opt = one_bit_lp(y, A, ~)
     [m, n] = size(A);
-
+    D = zeros(n-1, n);
+    for i = 1:n-1
+        D(i, i) = 1;
+        D(i, i+1) = -1;
+    end
+    lambda = 1;
+    cvx_clear
     cvx_begin quiet
-    
+        
        variable x(n)
            
-       minimize (norm(x, 1))
+       minimize (norm(x, 1) + lambda * norm(D * x, 1)) 
        subject to 
            y.*(A*x) >= 0;
            sum(y.*(A*x))/m >= 1;
